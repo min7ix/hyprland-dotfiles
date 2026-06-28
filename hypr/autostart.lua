411823e76d5514398716958e4858edcfaadaec1c
mintfr <mintix07@gmail.com>
2026-06-28 03:12:22 +0100

    tweaks to keybinds, added a resolution toggle

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -8,5 +8,6 @@ hl.on("hyprland.start", function ()
  hl.exec_cmd("waybar")
  hl.exec_cmd("swaync")
  hl.exec_cmd("swww-daemon")
-
+ hl.exec_cmd("ibus-daemon -rxRd")
+ 
 end)
--- hypr/env-variables.lua
+++ hypr/env-variables.lua
@@ -1,6 +1,14 @@
 
+
 -- Envirnoment Variables
 
 
 hl.env("XCURSOR_SIZE",    "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME",   "Breeze")
+
+hl.env("GTK_IM_MODULE",   "wayland")
+hl.env("QT_IM_MODULE",    "ibus")
+hl.env("XMODIFIERS",      "@im=ibus")
+hl.env("QT_IM_MODULES",   "wayland;ibus")
+
